f3804701be402c3b33d14e05a99de4e89285fda0
岡本能知 <daichiiiii317@icloud.com>
2024-08-31 14:56:32 +0900

    shift-dashboard

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
